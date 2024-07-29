6ff0d312a3a445c94d43f8000ad98753328b0827
Bar Rachmany <rachmanybar@gmail.com>
2024-07-29 16:44:44 +0300

    start

---
+++ salcal/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import BottomNavbar from './Components/BottomNavbar';
+
+function App() {
+  return (
+    <div className="app">
+      <BottomNavbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ salcal/src/Components/BottomNavbar.jsx
@@ -0,0 +1,11 @@
+const BottomNavbar = () => {
+    return (
+        <nav className="bottom-navbar">
+            <div>Work details</div>
+            <div>Shifts</div>
+            <div>Profile</div>
+        </nav>
+    );
+}
+
+export default BottomNavbar;
